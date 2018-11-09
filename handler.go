0a3c853307c9dc0a701c05c6916925a77dfe21ed
Kenaz <kenaz@puppetlabs.com>
2018-11-09 12:36:14 -0800

    Updating with types

---
+++ handler.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	log "github.com/Sirupsen/logrus"
+)
+
+// Handler interface contains the methods that are required
+type Handler interface {
+	Init() error
+	ObjectCreated(obj interface{})
+	ObjectDeleted(obj interface{})
+	ObjectUpdated(objOld, objNew interface{})
+}
+
+// TestHandler is a sample implementation of Handler
+type TestHandler struct{}
+
+// Init handles any handler initialization
+func (t *TestHandler) Init() error {
+	log.Info("TestHandler.Init")
+	return nil
+}
+
+// ObjectCreated is called when an object is created
+func (t *TestHandler) ObjectCreated(obj interface{}) {
+	log.Info("TestHandler.ObjectCreated")
+	log.Info("[genesis-log] Creating an instance of Genesis MyResource")
+	log.Info("[genesis-log] Creating genesis::aws::vpc")
+	log.Info("[genesis-log] Creating genesis::aws::securitygroup")
+	log.Info("[genesis-log] Creating genesis::aws::autoscalinggroup")
+	log.Info("[genesis-log] Created instance of Genesis MyResource")
+}
+
+// ObjectDeleted is called when an object is deleted
+func (t *TestHandler) ObjectDeleted(obj interface{}) {
+	log.Info("TestHandler.ObjectDeleted")
+	log.Info("[genesis-log] Deleting resources vpc, securitygroup")
+	log.Info("[genesis-log] Deleting resources autoscalinggroup") 
+}
+
+// ObjectUpdated is called when an object is updated
+func (t *TestHandler) ObjectUpdated(objOld, objNew interface{}) {
+	log.Info("TestHandler.ObjectUpdated")
+}
---
+++ pkg/apis/appstack/register.go
@@ -0,0 +1,4 @@
+package appstack
+
+const GroupName = "puppet.com"
+
